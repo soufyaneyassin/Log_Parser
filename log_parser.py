5f96e3948ebd6f1137b72be5e4a0ec1282a93dc8
soufyane <s.yassine@siliconesignal.com>
2025-10-31 10:58:24 +0000

    adding the parsing logic

--- log_parser.py
+++ log_parser.py
@@ -1,13 +1,21 @@
 from pathlib import Path
 import re
 
-log_file = Path("var/log/nginx.log")
+log_file = Path("/var/log/nginx.log")
 pattern = re.compile(r'(GET|PUT|POST|DELETE)\s.*"\s(\d{3})')
-codes_found = {}
+methods_found = {}
 try:
     with open(log_file) as file_obj:
          for line_number,line in enumerate(file_obj, 1):
-             print(f"Line  {line_number}, {line.strip()}")
+             line_result = pattern.findall(line.strip())
+             for method, code in line_result:
+                 if method not in methods_found:
+                      methods_found[method] = {}
+                 if code not in methods_found[method]:
+                      methods_found[method][code] = 1
+                 else:
+                      methods_found[method][code] += 1                      
+    print(methods_found)
 except FileNotFoundError:
      print(f"Error: file {log_file} Not found")
 except Exception as e:
